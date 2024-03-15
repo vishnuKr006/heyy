cd870e8292cd0ca8b3fe51cddddc6748b9b42d34
vishnuKr006 <162488939+vishnuKr006@users.noreply.github.com>
2024-03-15 10:09:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+alert("Accept all Cookies")
+
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
